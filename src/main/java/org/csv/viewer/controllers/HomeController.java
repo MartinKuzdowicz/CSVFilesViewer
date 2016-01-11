f0b6a2ac8a9b87b154d276cf00a153acc032e1a9
mkuzdowicz <mkuzdowic@power.com.pl>
2016-01-11 23:23:39 +0100

    first commit

---
+++ src/main/java/org/csv/viewer/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package org.csv.viewer.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/home")
+	public ModelAndView startPage() {
+
+		ModelAndView mav = new ModelAndView("ViewerMain");
+
+		return mav;
+
+	}
+
+}
